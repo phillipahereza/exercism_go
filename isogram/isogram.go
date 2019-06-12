8bd7628bed9b1b78e6cf8516a4eab426e0fe5c1a
Phillip Ahereza <ahereza@ahereza.local>
2019-06-12 14:07:25 +0300

    fix isIsogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,13 +1,21 @@
 package isogram
 
+import (
+	"regexp"
+	"strings"
+)
+
 // IsIsogram return true if string is isogram
-func IsIsogram(s string) bool{
-	words := make(map[rune]int)
-	duplicate := true
-	for _, v := range s {
-		_ = words
-		_ = v
-		
+func IsIsogram(s string) bool {
+	
+	freq := make(map[rune]rune)
+	reg, _ := regexp.Compile("[^a-zA-Z0-9]+")
+
+	processedString := reg.ReplaceAllString(strings.ToLower(s), "")
+	for _, r := range processedString {
+		freq[r] = r
 	}
-	return duplicate
+
+	return len(processedString) == len(freq)
+
 }
